a7406c53cb54edd0a8b265bb20df2a6c7ac5830c
Doreen Kendi Thuranira <kendid07@gmail.com>
2023-09-26 19:17:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ if (mainElement) {
 
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = 'Your-Name is the champion';
+newHeader.textContent = 'Reen is the champion';
 
 document.body.appendChild(newHeader);
